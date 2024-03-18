761edf620fc28e9884047566bd8b48cdc67d15bb
Tatjana Oljača <115995623+TackoBracko@users.noreply.github.com>
2024-03-18 13:29:25 +0100

    Add files via upload
    Scrip.js update

--- script.js
+++ script.js
@@ -1,96 +1,124 @@
 const date = document.querySelector('.date')
 date.innerText = new Date().toDateString()
 
-
-
 const activeTasksList = document.querySelector('.activeTasks')
 const completedTasksList = document.querySelector('.completedTasks')
 const inputField = document.querySelector('.taskInput')
 const submitBtn = document.querySelector('.submitBtn')
+const clearCompletedBtn = document.querySelector('.clearAll')
 const activeCountSpan = document.querySelector('.activeCount')
 const doneCountSpan = document.querySelector('.doneCount')
 
+/*activeTasksList.addEventListener('click', (e) => {
+    if (e.target.id && e.target.tagName === 'BUTTON') {
+      const button = document.getElementById(e.target.id)
+      const parent = button.parentElement
+      parent.remove()
+    }
+})*/
+
 let todoList = []
 
-function addTodoTask() {
+const addTodoTask = () => {
     if (inputField.value.trim() !== '') {
-        const todoItem = {
+        const task = {
             title: inputField.value,
             id: todoList.length + 1,
             status: 'active'
         }
         
-        todoList.push(todoItem)
+        todoList.push(task)
         console.log(todoList)
         inputField.value = ''
         
+        const newTask = createLiElement(task)
+        activeTasksList.appendChild(newTask)
         saveToLocalStorage()
-        renderTodoList() 
     }
 }
 
-function renderTodoList() {
+const createLiElement = (task) => {
+    const newTask = document.createElement('li')
+    newTask.innerText = task.title
+    newTask.dataset.id = task.id
+
+    const checkBox = document.createElement('input')
+    checkBox.type = 'checkbox'
+    checkBox.classList.add('checkbox')
+    checkBox.checked = task.status === 'done'
+    checkBox.addEventListener('change', () => toggleTodoStatus(task.id))
+
+    newTask.appendChild(checkBox)
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Delete'
+    deleteBtn.className = 'deleteBtn'
+    deleteBtn.addEventListener('click', () => {
+        todoList = todoList.filter((item) => item.id !== task.id)
+        saveToLocalStorage()
+    })
+
+    newTask.appendChild(deleteBtn)
+
+    return newTask
+}
+
+const renderTodoList = () => {
     activeTasksList.innerHTML = ''
     completedTasksList.innerHTML = ''
 
     let activeCount = 0
     let doneCount = 0
 
-    todoList.map(task => {
-        const newTask = document.createElement('li')
-        newTask.innerText = task.title
-        
-        const checkBox = document.createElement('input')
-        checkBox.type = 'checkbox'
-        checkBox.classList.add('checkbox')
-        checkBox.checked = task.status === 'done'
-        checkBox.addEventListener('change', () => toggleTodoStatus(task.id))
-
-        newTask.appendChild(checkBox)
-
-        const deleteBtn = document.createElement('button')
-        deleteBtn.textContent = 'Delete'
-        deleteBtn.className = 'deleteBtn'
-        deleteBtn.addEventListener('click', () => {
-            todoList = todoList.filter(item => item.id !== task.id)
-            saveToLocalStorage()
-            renderTodoList()
-        })
-
-        newTask.appendChild(deleteBtn)
+    todoList.map((task) => {
+        const newTask = createLiElement(task)
 
         if (task.status === 'done') {
             completedTasksList.appendChild(newTask)
-            doneCount++
+            doneCount++;
         } else {
             activeTasksList.appendChild(newTask)
-            activeCount++
+            activeCount++;
         }
     })
-    
+
     activeCountSpan.textContent = activeCount
     doneCountSpan.textContent = doneCount
 }
 
 const toggleTodoStatus = (id) => {
-    todoList.forEach(task => {
+    todoList.forEach((task) => {
         if (task.id === id) {
-            task.status === 'active' ? task.status='done' : task.status='active'
+            task.status === 'active' ? (task.status = 'done') : (task.status = 'active')
         }
-        return task
     })
     saveToLocalStorage()
     renderTodoList()
 }
 
+const removeTask = (e) => {
+    if (e.target.className === 'deleteBtn') {
+        const parent = e.target.parentElement
+        parent.remove()
+        saveToLocalStorage()
+    }
+}
+
+/*const deleteTask = (id) => {
+    todoList = todoList.filter(item => item.id !== id)
+    saveToLocalStorage()
+}*/
+
 submitBtn.addEventListener('click', addTodoTask)
+activeTasksList.addEventListener('click', removeTask)
+completedTasksList.addEventListener('click', removeTask)
 
 inputField.addEventListener('keypress', (e) => {
     if (e.keyCode === 13) {
-        addTodoTask() 
+        addTodoTask()
     }
 })
-
+  
 //localStorage
 
 const saveToLocalStorage = () => {
@@ -108,3 +136,4 @@ window.onload = () => {
     getFromLocalStorage()
     renderTodoList()
 }
+
