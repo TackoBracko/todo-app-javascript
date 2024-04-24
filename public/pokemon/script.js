d3dd079bc66a0051e42c2b33401f069410ce5de2
Tatjana Oljača <115995623+TackoBracko@users.noreply.github.com>
2024-04-24 16:42:41 +0200

    Add files via upload
    script.js

--- public/pokemon/script.js
+++ public/pokemon/script.js
@@ -1,4 +1,18 @@
 const getThatPokemon = document.querySelector('button')
+const errorMsg = document.querySelector('.place_for_errormsg')
+const inputForPokemon = document.querySelector('.pokemon_Input')
+const pokemonName = document.querySelector('.pokemon_Name')
+const pokemonHeight = document.querySelector('.pokemon_height')
+const pokemonAbility = document.querySelector('.pokemon_ability')
+const pokemonImg = document.querySelector('.pokemon_Pic')
+
+function clearUI() {
+    pokemonAbility.innerText = ''
+    pokemonHeight.innerText = ''
+    pokemonName.innerText = ''
+    pokemonImg.src = ''
+    errorMsg.innerText = ''
+}
 
 const setTimer = (duration) => {
     const promise = new Promise((resolve, reject) => {
@@ -8,52 +22,57 @@ const setTimer = (duration) => {
     })
     return promise
 }
+
 async function fetchPokemonData() {
     try {
+        clearUI()
         getThatPokemon.innerText = 'Loading...'
+        errorMsg.style.display = 'none'
         
         await setTimer(3000)
-
-        const inputForPokemon = document.querySelector('.pokemon_Input')
+        
         const fetchPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputForPokemon.value}`)
         
         if (fetchPokemon.status !== 200) {
-            throw new Error('You entered wrong Pokemon name')
+            //throw new Error (alert('You entered wrong Pokemon name'))
+            errorMsg.innerText = 'Hey, that pokemon does not exist'
+            errorMsg.style.display = 'block'
+            getThatPokemon.innerText = 'Get that Pokemon'
+            inputForPokemon.value = ""
         }
         
         const pokemon = await fetchPokemon.json()
         
         //pokemonInfo
-
-        const pokemonName = document.querySelector('.pokemon_Name')
+        
         pokemonName.innerText = pokemon.name
-
-        const pokemonHeight = document.querySelector('.pokemon_height')
+        
         pokemonHeight.innerText = `height: ${pokemon.height}`
         pokemonHeight.style.display = 'block'
-
-        const pokemonAbility = document.querySelector('.pokemon_ability')
+        
         pokemonAbility.innerText = `abilities: ${pokemon.abilities.map(ability => ability.ability.name)}`
         pokemonAbility.style.display = 'block'
-
-        const pokemonImg = document.querySelector('.pokemon_Pic')
+        
         pokemonImg.src = pokemon.sprites.front_default
-
+        
         getThatPokemon.innerText = 'Get that Pokemon'
         inputForPokemon.value = ''
-
+        
         console.log(pokemon)
     }
     catch(error) {
-        console.error(error)
+        console.error('Wrong Pokemon name')
     }
 }
 
-getThatPokemon.addEventListener('click', fetchPokemonData)
-
-document.querySelector('.pokemon_Input').addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
+inputForPokemon.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' && inputForPokemon.value.trim() !== '') {
         e.preventDefault()
         fetchPokemonData()
     }
+})
+
+getThatPokemon.addEventListener('click', () => {
+    if (inputForPokemon.value.trim() !== '') {
+        fetchPokemonData}
 })
